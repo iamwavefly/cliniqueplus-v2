176942ea94d303eef5462a62a6d75590842da1b6
wavefly <iamwavefly@gmail.com>
2021-01-14 14:15:09 +0100

    dashboard v6

---
+++ public/scripts/status.js
@@ -0,0 +1,32 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    // Autoplay
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    cubeEffect: {
+      slideShadows: false,
+    },
+    effect: "fade",
+    speed: 2000,
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
