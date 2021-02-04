519090de90f57297e681cb9d6fe22fc146a87161
wavefly <iamwavefly@gmail.com>
2021-02-04 09:56:49 +0100

    add hamburger

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,20 @@
 // DOM
-const leftToggle = document.querySelector("#left_toggle")
-const cola = document.querySelector("#cola")
-const navLink = document.querySelectorAll("#nav_link")
 let btnContent = document.querySelector("#btn__content") 
 let logoContent = document.querySelector("#logo__content") 
 let primaryBtn = document.querySelector("#primary_btn") 
 let brandLogo = document.querySelector("#brand_logo") 
+const leftToggle = document.querySelector("#left_toggle")
+const cola = document.querySelector("#cola")
+const navLink = document.querySelectorAll("#nav_link")
 const navbar = document.querySelectorAll("#navbar") 
 const navbarList = document.querySelectorAll("#nav_list") 
 const dropNav = document.querySelectorAll("#drop__nav") 
 const dropArr = document.querySelectorAll("#drop__arr") 
-const snippetDrop = document.querySelector("#user__snippet__arr") 
+const snippetDrop = document.querySelectorAll("#user__snippet__arr") 
 const userSnippetPanel = document.querySelector("#user__snippet__panel") 
+const hamburger = document.querySelector("#hamburger") 
+const MsgCont = document.querySelectorAll("#MsgCont") 
+const msgContent = document.querySelectorAll("#msgContent") 
 // Functions
 const colaFunc = () => {
     navLink.forEach(link => {
@@ -40,20 +43,26 @@ dropNav.forEach(drop => {
     drop.classList.add("display__close")
 })
 const snippetDropFunc = (e) => {
-    userSnippetPanel.classList.add("snippet__panel-red")
+    userSnippetPanel.classList.toggle("snippet__panel-red")
+    setTimeout(() => {
+        userSnippetPanel.classList.remove("snippet__panel-red")
+    }, 5000);
 }
-const bodyFunc = (e) => {
-    userSnippetPanel.classList.contains("snippet__panel-red") && userSnippetPanel.classList.remove("snippet__panel-red")
+const handleHamburger = (e) => {
+    hamburger.getAttribute("src") == "./assets/icons/hamburger.svg" ? hamburger.setAttribute("src", "./assets/icons/cola_open.svg") : hamburger.setAttribute("src", "./assets/icons/hamburger.svg")
+    leftToggle.classList.toggle("mobile__menu-display")
 }
 // Events
 cola.addEventListener("click", colaFunc)
 leftToggle.addEventListener("mouseenter", colaHoverFunc)
-snippetDrop.addEventListener("mouseenter", snippetDropFunc)
-document.querySelector("body").addEventListener("click", bodyFunc)
+hamburger.addEventListener("click", handleHamburger)
 
 // --------------------------------------------
-// Beware!!! OOP
 // --------------------------------------------
+// LOOP
+snippetDrop.forEach(drop => {
+    drop.addEventListener("click", snippetDropFunc)
+})
 navbarList.forEach((nav, index) => {
     nav.addEventListener("click", (e)=> {
         const ul = nav.nextElementSibling
@@ -66,3 +75,23 @@ navbarList.forEach((nav, index) => {
 })
 
 
+// --------------------------------------------------------------
+// --------------------------[LEV1]------------------------------
+// --------------------------------------------------------------
+// const handleClick = (e) =>{
+//     msgContent.forEach(msg => {
+//         // msg.classList.toggle("show__msg")
+
+//     })
+// }
+const msgShower = (msg, index) => {
+    console.log(index);
+    msg.addEventListener("click", () => {
+        msgContent[index].classList.toggle("show__msg")
+    })
+}
+const showBox = (message) => {
+    message.forEach(msgShower)
+}
+
+showBox(MsgCont)
